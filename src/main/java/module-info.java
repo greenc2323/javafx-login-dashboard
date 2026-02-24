4ef3b57885e2800e422040154aef1d1cbfe8fa0d
CainGreen <caingreen2323@gmail.com>
2026-02-24 12:08:24 -0600

    Update module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,9 +1,9 @@
-module gfj947_lab4 {
+module abc123_lab4 {
     requires javafx.controls;
     requires javafx.fxml;
 
-    opens com.cain.green.javafxlogindashboard to javafx.fxml;
-    opens com.cain.green.javafxlogindashboard.model to javafx.fxml;
+    opens com.first.last.javafxlogindashboard to javafx.fxml;
+    opens com.first.last.javafxlogindashboard.model to javafx.fxml;
 
-    exports com.cain.green.javafxlogindashboard;
+    exports com.first.last.javafxlogindashboard;
 }
